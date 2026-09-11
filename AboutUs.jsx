ce5e19738f5dab9ea2795db413730949cafeee0a
Avdhesh2006 <avdheshvg@gmail.com>
2026-09-11 22:33:00 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1 className="about-us-heading">About Us</h1>
+      <p className="about-us-description">
+        Welcome to Paradise Nursery, where green dreams come true! We are passionate about bringing nature closer to you.
+      </p>
+      <p className="about-us-content">
+        Our mission is to provide high-quality houseplants that elevate your living space, purify your indoor air, and provide a sense of calm and well-being.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
